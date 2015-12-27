58ace4deacc8a993d1612a639b0a5af932bcc3b3
Michael Lehmann <leh@zuehlke.com>
2015-12-27 15:18:26 +0100

    feat(api): Prepare some basic structure for the api.

---
+++ src/Zuehlke.AppMonitor.Server/Api/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zuehlke.AppMonitor.Server.Api.Models
+{
+    public class Project
+    {
+    }
+}
